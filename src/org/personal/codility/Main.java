7f090ca1ea4a646099b1265a3760fed5d7de1db9
FilipChromy <Filip.Chromy@homecredit.eu>
2021-10-02 18:29:47 +0200

    pushing all files

---
+++ src/org/personal/codility/Main.java
@@ -0,0 +1,20 @@
+package org.personal.codility;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class BinaryGap {
+
+}
+
+class CyclicRotation {
+
+}
+
+class OddOccurrencesInArray {
+
+}
