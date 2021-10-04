a246df0d148fd7300a32f206117de9bda18810e1
FilipChromy <Filip.Chromy@homecredit.eu>
2021-10-04 17:05:32 +0200

    CyclicRotation solved

--- src/org/personal/codility/Main.java
+++ src/org/personal/codility/Main.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	    BinaryGap gap = new BinaryGap();
-	    System.out.println(gap.solution(1041));
+	    CyclicRotation cycleTwo = new CyclicRotation();
+        System.out.println(Arrays.toString(cycleTwo.solution(new int[] {1, 2, 3, 4, 5}, 2)));
     }
 }
 
 class BinaryGap {
+
     int solution(int A) {
         String binaryString = Integer.toBinaryString(A);
         int counter = 0;
@@ -37,6 +38,17 @@ class BinaryGap {
 
 class CyclicRotation {
 
+    int[] solution(int A[], int B) {
+        int[] rotatedArray = new int[A.length];
+        for (int i=0; i<A.length; i++) {
+            int rotate = B + i;
+            while (rotate >= A.length) {
+                rotate -= A.length;
+            }
+            rotatedArray[rotate] = A[i];
+        }
+        return rotatedArray;
+    }
 }
 
 class OddOccurrencesInArray {
