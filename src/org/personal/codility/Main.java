34cf2462f5327ad59a4d798bda3e03fcb1e0c128
FilipChromy <Filip.Chromy@homecredit.eu>
2021-10-04 11:19:56 +0200

    BinaryGap finished

--- src/org/personal/codility/Main.java
+++ src/org/personal/codility/Main.java
@@ -1,14 +1,38 @@
 package org.personal.codility;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    BinaryGap gap = new BinaryGap();
+	    System.out.println(gap.solution(1041));
     }
 }
 
 class BinaryGap {
-
+    int solution(int A) {
+        String binaryString = Integer.toBinaryString(A);
+        int counter = 0;
+        int maxGap = 0;
+        boolean started = false;
+        for (int i=0; i<binaryString.length(); i++) {
+            String c = binaryString.substring(i, i+1);
+            if (c.equals("1")) {
+                if (started) {
+                    if (counter > maxGap) {
+                        maxGap = counter;
+                    }
+                }
+                started = true;
+                counter = 0;
+            }
+            if (c.equals("0")) {
+                counter++;
+            }
+        }
+        return maxGap;
+    }
 }
 
 class CyclicRotation {
